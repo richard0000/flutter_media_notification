3f2cefb5e8d20e75a2ceec1254db5e8e83dd7936
Richard G <r.richard0000@gmail.com>
2020-03-03 15:32:40 -0300

    Corrections to have the image received as a String again

--- lib/media_notification.dart
+++ lib/media_notification.dart
@@ -26,33 +26,46 @@ class MediaNotification {
   }
 
   ///  params @BitmapImage a list of bytes representing an image, has less priority when @image is present
-  static Future show(
-      {@required title, @required author, play = true, ByteData image, List<
-          int> bitmapImage, Color bgColor, Color titleColor, Color subtitleColor, Color iconColor, Icon previousIcon}) async {
-    /// switching the image from a URI to a byteArray for Android with offset and length;
-    List<int> imagebytes = byteDatatoList(image);
+  static Future show({
+    @required title,
+    @required author,
+    play = true,
+    String image,
+    List<int> bitmapImage,
+    Color bgColor,
+    Color titleColor,
+    Color subtitleColor,
+    Color iconColor,
+    Icon previousIcon}) async {
 
-    final Map<String, dynamic> params = <String, dynamic>{
+    /// Switching the image from a URI to a byteArray for Android with offset and length;
+    ByteData bytesImage = await rootBundle.load('assets/images/icon.png');
+    List<int> imagebytes = byteDatatoList(bytesImage);
+
+    final Map<String, dynamic> params = <String, dynamic> {
       'title': title,
       'author': author,
       'play': play,
-      'image': imagebytes != null ? imagebytes : bitmapImage,
-      'length': imagebytes != null ? imagebytes.length : bitmapImage != null
-          ? bitmapImage.length
-          : 0,
+      'image': (imagebytes != null) ? imagebytes : bitmapImage,
+      'length': (imagebytes != null) ? imagebytes.length : ((bitmapImage != null) ? bitmapImage.length: 0),
       'offset': 0,
-      'bgColor': bgColor != null
+
+      'bgColor': (bgColor != null)
           ? '#${bgColor.value.toRadixString(16)}'
           : '#${Colors.white.value.toRadixString(16)}',
-      'titleColor': titleColor != null ? '#${titleColor.value.toRadixString(
-          16)}' : '#${Colors.black.value.toRadixString(16)}',
-      'subtitleColor': subtitleColor != null ? '#${subtitleColor.value
-          .toRadixString(16)}' : '#838383',
+
+      'titleColor': (titleColor != null) ?
+          '#${titleColor.value.toRadixString(16)}' : '#${Colors.black.value.toRadixString(16)}',
+
+      'subtitleColor': (subtitleColor != null) ?
+          '#${subtitleColor.value.toRadixString(16)}' : '#838383',
+
       /// This color is arbitrary and can be changed but it keeps a good look
       'iconColor': iconColor != null
           ? '#${iconColor.value.toRadixString(16)}'
           : '#${Colors.black.value.toRadixString(16)}',
     };
+
     await _channel.invokeMethod('show', params);
 
     _channel.setMethodCallHandler(_myUtilsHandler);
